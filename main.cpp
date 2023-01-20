be7d85ee6d5899a36194f98cf7105869e6eec337
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-01-20 15:20:04 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()  {
+  cout << "---------------------------------------"<< endl;
+  cout << "Jack R" << endl;
+  cout << "comp Sci 1"<< endl;
+  cout << "Orange county community college" << endl;
+  cout << "lab1" << endl;
+  cout << "January 20th, 2023"<< endl;
+  cout << "---------------------------------------"<< endl;
+  int x; // integer variable
+  int y; // integer variable;
+  x =  20;
+  //cout<<"content of x right after initialization: " << x << endl;
+  y = 5;
+  cout<<"x+y= "<< x+y << endl;
+  //cout<<"content of y right before program exit: " <<y<< endl;
+
+  return 0;
+}
