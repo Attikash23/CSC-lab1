37bdf7c77fd1a88583fdd9ec377a02237b122377
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 22:42:15 -0400

    Add files via upload

---
+++ LM 01 code.cpp
@@ -0,0 +1,58 @@
+
+// Jack Rein
+#include <iostream> 
+using namespace std;
+int main ()
+{
+  // LM 1.1
+/*cout << "Now is the time for all good men" << endl;
+cout << "To come to the aid of their party" << endl;
+return 0;*/
+
+  //Lm 1.2
+ /* int number;
+float total;
+cout << "Today is a great day for Lab";
+cout << endl << "Let's start off by typing a number of your choice" << endl;
+cin >> number;
+total = number * 2;
+cout << total << " is twice the number you typed" << endl;
+return 0;*/
+
+//Lm 1.3
+ /* float number;
+int divider;
+divider = 2;
+cout << "Hi there" << endl;
+cout << "Please input a number and then hit return" << endl;
+cin >> number;
+number = number / divider;
+cout << "Half of your number is " << number << endl;
+return 0;*/
+
+
+  //Lm 1.4
+ /* float firstNumber;
+float secondNumber;
+cout << "Enter the first number" << endl;
+cout << "Then hit enter" << endl;
+cin >> firstNumber;
+cout << "Enter the second number" << endl;
+cout << "Then hit enter" << endl;
+cin >> secondNumber;
+cout << endl << "You input the numbers as " << firstNumber
+<< " and " << secondNumber << endl;
+cout << "After swapping, the values of the two numbers are "
+<< secondNumber << " and " << firstNumber << endl;
+return 0;*/
+
+
+
+
+
+
+
+
+
+  
+}
